4b63b7e0f2c2a7d8379bd57d3e0bde7400b2d8be
Mateusz Ostrowski <mateusz.ostro@o2.pl>
2025-04-27 22:19:29 +0200

    declared class for file explorer

---
+++ FileExplorer/Components/FileExplorer.cs
@@ -0,0 +1,27 @@
+﻿#region Using Statements
+
+using FileExplorer.Components.DirectoryComponents;
+
+#endregion
+
+namespace FileExplorer.Components
+{
+    public class FileExplorer : ViewModelBase
+    {
+        #region Properties
+
+        public DirectoryInfoViewModel? Root { get; set; }
+
+        #endregion
+
+        #region Public Methods
+
+        public void OpenRoot(string path)
+        {
+            Root = new DirectoryInfoViewModel();
+            Root.Open(path);
+        }
+
+        #endregion
+    }
+}
