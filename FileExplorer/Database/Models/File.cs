c3bb76996cdd3acaa1ff289efcd1d9e6f7f0a484
Mateusz Ostrowski <mateusz.ostro@o2.pl>
2025-06-08 23:37:44 +0200

    update

---
+++ FileExplorer/Database/Models/File.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FileExplorer.Database.Models
+{
+    public class File
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string FullPath { get; set; }
+
+        [MaxLength(50)]
+        public string Description { get; set; }
+
+        public User CreatedBy { get; set; }
+
+        public List<FileHistory> Modifications { get; set; }
+    }
+}
---
+++ FileExplorer/Database/Models/FileHistory.cs
@@ -0,0 +1,12 @@
+﻿namespace FileExplorer.Database.Models
+{
+    public class FileHistory
+    {
+        public int Id { get; set; }
+        public File File { get; set; }
+        public User ModifiedBy { get; set; }
+        public DateTime ModifiedAt { get; set; }
+        public string ModificationNote { get; set; }
+
+    }
+}
